e9aeb9b84dd66bde40f665bebe9750aafeb1cb14
Doug Rosman <drosman@saic.edu>
2018-09-21 16:47:26 -0500

    Update harmonic motion and other examples

--- Extra-Examples/harmonic_motion/src/ofApp.cpp
+++ Extra-Examples/harmonic_motion/src/ofApp.cpp
@@ -1,52 +1,83 @@
 #include "ofApp.h"
 
 
+// NOTE: once you run the sketch, click the green dot in the corner of your window
+// to make it fullscreen!
+
 void ofApp::setup()
 {
     ofBackground(0);
     ofSetCircleResolution(80);
     circleSize = 2;
-    //ofSetBackgroundAuto(false);
-}
-
-void ofApp::update()
-{
-
 }
 
 void ofApp::draw()
 {
-    
+    // Create variables w and h to hold screen width and height values
     float w = ofGetWidth();
     float h = ofGetHeight();
     
-    
-    
+    // How far apart circles are drawn
     float spacing = circleSize*2;
+    
+    // Sets the limit for the right-most circle to be drawn.
     float rightLimit = w - spacing + 1;
     
-    //ofSetColor(0);
-    
+    // Use a for-loop to draw the dots.
+    // Why i = spacing?: doesn't start by drawing the circle right on the left edge of the screen
+    // Why i < rightLimit?: I don't want to draw circles on the right edge of the screen
+    // Why i+=spacing?: I want to draw evenly-spaced circles based on the 'spacing' value I set
     for(int i = spacing; i < rightLimit; i+=spacing)
     {
         float t = ofGetElapsedTimef();
-        float phase = i/spacing;
+        
+        // Base the hue value of the each circle on its vertical position
         float hue = ofMap(sin(ofRadToDeg(t*i/rate))*heightRange, -heightRange, heightRange, 0, 255);
+        
         ofPushMatrix();
+        // Translate the x-value of each dot based on spacing, translate the y-value to the center of the screen.
         ofTranslate(i, h/2);
-        ofSetColor(ofColor::fromHsb(hue, sat, 255, alpha));
-        ofDrawCircle(0, sin(ofRadToDeg(t*i/rate))*heightRange, (circleSize*circleSizeMult)/2);
         
-//        ofDrawLine(0, sin(ofRadToDeg(t*i/rate))*heightRange, 0, sin(ofRadToDeg(t*(i+spacing)/rate))*heightRange);
-
-        ofSetLineWidth(lineWidth);
-        ofDrawLine(cos(ofRadToDeg(t*i/rate))*heightRange, sin(ofRadToDeg(t*i/rate))*heightRange, cos(ofRadToDeg(t*(i+spacing)/rate))*heightRange, sin(ofRadToDeg(t*(i+spacing)/rate))*heightRange);
+            // Set the color of the circles/lines
+            ofSetColor(ofColor::fromHsb(hue, sat, 255, alpha));
         
+            if(drawCircles)
+            {
+                // Draw the circle based on its rate and point in the loop.
+                ofDrawCircle(0, sin(ofRadToDeg(t*i/rate))*heightRange, (circleSize*circleSizeMult)/2);
+            }
+    
+            if(drawLines)
+            {
+                // Draw lines based on dot positions, with some extra cosine fun thrown in for the x-values
+                ofSetLineWidth(lineWidth);
+                ofDrawLine(cos(ofRadToDeg(t*i/rate))*heightRange, sin(ofRadToDeg(t*i/rate))*heightRange, cos(ofRadToDeg(t*(i+spacing)/rate))*heightRange, sin(ofRadToDeg(t*(i+spacing)/rate))*heightRange);
+            }
         ofPopMatrix();
         //std::cout << sin(ofRadToDeg(t*i/20000))*200 << std::endl;
     }
+    
+    // Display interaction functions
+    
+    if(drawInstructions)
+    {
+        ofDrawBitmapStringHighlight("Press 'H' to show or hide these instructions", 20, 40);
+        ofDrawBitmapStringHighlight("Each parameter can be increased or decreased by pressing or holding down its corresponding keys", 20, 55);
+        ofDrawBitmapString("------------------------------------------------------------------------------------------------", 20, 70);
+        ofDrawBitmapStringHighlight("Size/Spacing: " + ofToString(circleSize) + " | '<' , '>'", 20, 90);
+        ofDrawBitmapStringHighlight("Amplitude: " + ofToString(heightRange) + " | '[' , ']'", 20, 105);
+        ofDrawBitmapStringHighlight("Rate: " + ofToString(rate) + " | '9' , '0'", 20, 120);
+        ofDrawBitmapStringHighlight("Size multiplier: " + ofToString(circleSizeMult) + " | '-' , '+'", 20, 135);
+        ofDrawBitmapStringHighlight("Alpha (opacity): " + ofToString(alpha) + " | 'A' , 'S'", 20, 150);
+        ofDrawBitmapStringHighlight("Saturation: " + ofToString(sat) + " | 'Q' , 'W'", 20, 165);
+        ofDrawBitmapStringHighlight("Line Thickness: " + ofToString(lineWidth) + " | 'Z' , 'X'", 20, 180);
+        ofDrawBitmapStringHighlight("Draw Circles: " + ofToString(drawCircles) + " | 'C'", 20, 195);
+        ofDrawBitmapStringHighlight("Draw Lines: " + ofToString(drawLines) + " | 'L'", 20, 210);
+    }
+    
 }
 
+// Set up key pressed to allow for manipulating parameters in real time
 void ofApp::keyPressed(int key)
 {
     if(key == ',' && circleSize > 1)
@@ -60,12 +91,12 @@ void ofApp::keyPressed(int key)
     
     else if(key == '[' && heightRange > 1)
     {
-        heightRange-=2;
+        heightRange-=4;
     }
     
     else if(key == ']' && heightRange < 1440)
     {
-        heightRange+=2;
+        heightRange+=4;
     }
     
     else if(key == '9' && rate > 1001)
@@ -73,7 +104,7 @@ void ofApp::keyPressed(int key)
         rate-=1000;
     }
     
-    else if(key == '0' && rate < 100000)
+    else if(key == '0' && rate < 150000)
     {
         rate+=1000;
     }
@@ -118,6 +149,19 @@ void ofApp::keyPressed(int key)
         lineWidth+=1;
     }
     
+    else if (key == 'l')
+    {
+        drawLines = !drawLines;
+    }
+    
+    else if (key == 'c')
+    {
+        drawCircles = !drawCircles;
+    }
+    else if (key == 'h')
+    {
+        drawInstructions = !drawInstructions;
+    }
     
 }
 
--- Extra-Examples/harmonic_motion/src/ofApp.h
+++ Extra-Examples/harmonic_motion/src/ofApp.h
@@ -6,17 +6,21 @@ class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
-		void update();
 		void draw();
 		
     void keyPressed(int key);
     
-    float circleSize = 0;
-    float heightRange = 1;
-    float rate = 10000;
-    float circleSizeMult = 1;
-    float alpha = 255;
-    float sat = 255;
-    float lineWidth = 1;
     
+    // Create variables that allow you to manipulate the sketch in real-time.
+    
+    float circleSize = 0;   // size of each circle. Used to determine spacing between circles
+    float heightRange = 1;  // height of the sine wave oscillations
+    float rate = 10000; // how quickly the sine wave cycles
+    float circleSizeMult = 1; // enlarges the circles without affecting spacing
+    float alpha = 255;  // allows you to change the alpha channel (opacity) of the circles
+    float sat = 255; // allows you to change the saturation
+    float lineWidth = 1; // allows you to change line thickness
+    bool drawCircles = true;
+    bool drawLines = false;
+    bool drawInstructions = true;
 };
